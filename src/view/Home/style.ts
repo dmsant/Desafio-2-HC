2123217cb12369c2f79a6799d6d85f969963b2d2
Daniel Moreira <daniell.msantos@gmail.com>
2021-07-21 23:57:55 -0300

    Projeto do desafio 2 do Hiring Coders explicado pelo Douglas na mentoria

---
+++ src/view/Home/style.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 80%;
+    margin: auto;
+
+    section{
+        display: flex;
+        height: 100vh;
+        width: 100%;
+        background: #e2e2e2;
+    }
+
+    .product-content{
+        display: grid;
+        text-align: center;
+        height: 300px;
+        background: #fff;
+        border-radius: 12px;
+        padding: 12px;
+    }
+
+`
