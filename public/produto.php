9abb1f299c5ab1c3719c0c8a89bb4a09e8465ca9
Daniel Moreira <daniell.msantos@gmail.com>
2021-07-22 23:05:06 -0300

    commit feito depois do de alteração no php

--- public/produto.php
+++ public/produto.php
@@ -1,5 +1,4 @@
-{
-    "produtos":[
+[
         {
             "id":1,
             "photo":"https://a-static.mlcdn.com.br/618x463/iphone-11-apple-64gb-preto-61-12mp-ios/magazineluiza/155610500/2815c001fcdff11766fcb266dca62daf.jpg",
@@ -15,4 +14,3 @@
             "price": 4000
         }
     ]
-}
